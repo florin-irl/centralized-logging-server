a383809a83c67683ee37229ee8fc839cbeac23ba
Ivana Florin-Andrei <ivana.florin1987@gmail.com>
2025-07-19 22:54:27 +0300

    log4j2

---
+++ version-2/CharlieService/src/main/java/com/example/charlieservice/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.example.charlieservice.config;
+
+import com.example.charlieservice.logging.RequestResponseLoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final RequestResponseLoggingInterceptor loggingInterceptor;
+
+    @Autowired
+    public WebMvcConfig(RequestResponseLoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
