caa4d8a2b240f5efc33809afded5157f11dfb2ae
Ivana Florin-Andrei <ivana.florin1987@gmail.com>
2025-07-23 00:42:43 +0300

    nginx logs get posted only on container restart

--- version-2/BetaService/obj/Debug/net8.0/BetaService.AssemblyInfo.cs
+++ version-2/BetaService/obj/Debug/net8.0/BetaService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetaService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fabbc0536cbd814a35cb72107576fda1e4a55a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a11e493112e2338f9568d2e303444327a23188")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetaService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetaService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
