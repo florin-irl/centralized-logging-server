a1324163b5e72f4af3ab2ec563bc91bbd6bbd1cf
Ivana Florin-Andrei <ivana.florin1987@gmail.com>
2025-08-03 23:15:23 +0300

    P0 COMPLETE -> can trace using trace.id

--- version-2/AlphaService/obj/Debug/net8.0/AlphaService.AssemblyInfo.cs
+++ version-2/AlphaService/obj/Debug/net8.0/AlphaService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlphaService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b110dcc048b124a7135b3d16a133daef4f4842e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb2bbf007c79f6aa458b96137762e501ea861b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlphaService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlphaService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- version-2/BetaService/obj/Debug/net8.0/BetaService.AssemblyInfo.cs
+++ version-2/BetaService/obj/Debug/net8.0/BetaService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetaService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b110dcc048b124a7135b3d16a133daef4f4842e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb2bbf007c79f6aa458b96137762e501ea861b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetaService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetaService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
